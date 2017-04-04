57f850a2443c495d71148fd89521b905c2da00cc
Pablo <daytightchuncks@gmail.com>
2017-04-04 09:30:41 +0200

    fig update

--- SavingPackagesBeforeUpdate.R
+++ SavingPackagesBeforeUpdate.R
@@ -1,17 +1,23 @@
 
 getwd()
-setwd("D:/Documents/these_pablo/Alteckendorf2016/HydrologicalMonitoring")
 
 tmp <- installed.packages()
 installedpkgs <- as.vector(tmp[is.na(tmp[,"Priority"]), 1])
+
+setwd("D:/Documents/these_pablo/Alteckendorf2016/HydrologicalMonitoring")
 save(installedpkgs, file="installed_old.rda")
+
+setwd("/Users/DayTightChunks/Documents/PhD/HydrologicalMonitoring")
+save(installedpkgs, file="installed_old_mac.rda")
 
 # After laoding new R version
 
 getwd()
 setwd("D:/Documents/these_pablo/Alteckendorf2016/HydrologicalMonitoring")
-
 load("installed_old.rda")
+setwd("/Users/DayTightChunks/Documents/PhD/HydrologicalMonitoring")
+load("installed_old_mac.rda")
+
 tmp <- installed.packages()
 installedpkgs.new <- as.vector(tmp[is.na(tmp[,"Priority"]), 1])
 missing <- setdiff(installedpkgs, installedpkgs.new)
